cec9a5598ff31b72a58c8f313d97a989cd56f3ee
belinyak <belinyakmate@Belinyaks-MacBook-Pro.local>
2016-02-07 01:28:30 +0100

    # Versenysorozat panel megjelenítés

--- Ijasz2/Ijasz2/Megjelenites/Versenysorozat/Versenysorozat_Hozzaadas.xaml.cs
+++ Ijasz2/Ijasz2/Megjelenites/Versenysorozat/Versenysorozat_Hozzaadas.xaml.cs
@@ -10,20 +10,15 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Ijasz2 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Versenysorozat_Hozzaadas.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow( ) {
+    public partial class Versenysorozat_Hozzaadas : Window {
+        public Versenysorozat_Hozzaadas( ) {
             InitializeComponent( );
-        }
-
-        private void btnHozzaadas_Click( object sender, RoutedEventArgs e ) {
-            ( new Versenysorozat_Hozzaadas( ) ).ShowDialog( );
         }
     }
 }
